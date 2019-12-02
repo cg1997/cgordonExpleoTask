353c3dafb05867417c70e7e0f5a1a9985b27729b
cg1997 <craigcg2197@gmail.com>
2019-12-02 22:02:31 +0000

    Fourth Test Class completed + message fixes

--- ExpleoTask/src/TC04_AlertButtonLoadTest.java
+++ ExpleoTask/src/TC04_AlertButtonLoadTest.java
@@ -2,13 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TC03_ButtonLoadTest {
+public class TC04_AlertButtonLoadTest {
 
     public static void main(String[] args) throws Exception{
 
         String baseUrl = "https://the-internet.herokuapp.com/challenging_dom";
-        String expectedButtonLabel = "button";
-        String actualButtonLabel = "";
+        String expectedAlertButtonLabel = "button alert";
+        String actualAlertButtonLabel = "";
 
         //Locates chrome webdriver
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
@@ -17,13 +17,13 @@ public class TC03_ButtonLoadTest {
         //driver goes to webpage
         driver.get(baseUrl);
 
-        //Grabs string value from the button element by finding it by the xpath
-        actualButtonLabel = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[1]/a[1]")).getAttribute("class");
+        //Grabs string value from the alert button element by finding it by the xpath
+        actualAlertButtonLabel = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[1]/a[2]")).getAttribute("class");
 
-        if (actualButtonLabel.contentEquals(expectedButtonLabel)){
-            System.out.println("Message: Passed - Button Text Found");
+        if (actualAlertButtonLabel.contentEquals(expectedAlertButtonLabel)){
+            System.out.println("Message: Passed - Alert Button Text Found");
         } else {
-            System.out.println("Message: Failed - Button Text Not Found");
+            System.out.println("Message: Failed - Alert Button Text Not Found");
         }
 
         //After the result the driver is closed
